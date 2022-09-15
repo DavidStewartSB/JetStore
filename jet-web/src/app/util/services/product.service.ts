d4d144ac22c75f81088d48fbe2c308b3958ddc57
David Stewart <david.stewartsb@gmail.com>
2022-09-15 13:15:56 -0300

    add: all httpClient on services

--- jet-web/src/app/util/services/product.service.ts
+++ jet-web/src/app/util/services/product.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 
-
 @Injectable({
     providedIn: 'root'
 })
@@ -15,4 +14,20 @@ export class ProductsService {
     getProducts(): Observable<Product[]>{
         return this.http.get<Product[]>(this.apiURLProducts)
     }
+    getProduct(productId: String): Observable<Product>{
+        return this.http.get<Product>(`${this.apiURLProducts}/${productId}`)
+    }
+    getProductCount(): Observable<number> {
+        return this.http.get<number>(`${this.apiURLProducts}/get/count`)
+    }
+    createProduct(productData: FormData): Observable<Product>{
+        return this.http.post<Product>(`${this.apiURLProducts}`, productData)
+    }
+    updateProduct(productFormData: FormData, productId: string): Observable<Product>{
+        return this.http.put<Product>(`${this.apiURLProducts}/${productId}`, productFormData)
+    }
+    deleteProduct(product: Product): Observable<Product> {
+        return this.http.delete<Product>(`${this.apiURLProducts}/${product}`)
+    }
+
 }
